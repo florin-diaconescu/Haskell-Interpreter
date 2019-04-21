6d43b31937c76f6c1cb4a43993c75f3950d6dad0
Florin Diaconescu <33086960+houstain@users.noreply.github.com>
2019-04-21 15:41:11 +0300

    61.5 points, bread incoming

--- Parser.hs
+++ Parser.hs
@@ -8,6 +8,8 @@ import InferenceDataType
 import Data.Map (Map)
 import qualified Data.Map as Map
 
+import Data.Char
+
 -- Definire Program
 data Program = Program (Map String [[String]]) deriving Show
 
@@ -36,14 +38,28 @@ getFuncsForClass clsname (Program prog)
           funcList = [xs | x1:x2:xs <- listClasses, x2 ==  clsname]
 
 -- Instruction poate fi ce considerați voi
-data Instruction = Instruction
+type Instruction = String
+
 parse :: String -> [Instruction]
-parse = undefined
+parse input = lines input
+
+lowerString :: String -> String
+lowerString str = [(toLower chr) | chr <- str]
 
 interpret :: Instruction -> Program -> Program
-interpret = undefined
+interpret instr (Program pr)
+    | (length (words instr)) == 0 = Program pr
+    | (lowerString (head (words instr)) == "class") && ((length (words instr)) == 4) =
+        Program (Map.insertWith (++) "Cls" [(words instr)!!3 : [(words instr)!!1]] pr)
+
+    | (lowerString (head (words instr)) == "class") &&  ((length (words instr)) == 2) =
+        Program (Map.insertWith (++) "Cls" ["Global": [words(instr)!!1]] pr)
+
+    | (lowerString (head (words instr)) == "newvar") && ((length (words instr)) == 4) &&
+       ((words instr)!!3 `elem` (getClasses (Program pr))) =
+       Program (Map.insertWith (++) "Var" [(words instr)!!1 : [(words instr)!!3]] pr)
+
+    | otherwise = Program pr
 
 infer :: Expr -> Program -> Maybe String
-infer = undefined
-
-
+infer _ _ = Nothing
